caf2803f88d2d2dd034db96c4c36d9edc3586e0c
Kyle Meyer <vegas.kylem@gmail.com>
2025-04-21 02:14:40 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,263 @@
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_win32.h"
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include <GL/GL.h>
+#include <tchar.h>
+
+// Data stored per platform window
+struct WGL_WindowData { HDC hDC; };
+
+// Constants
+static const POINT      k_WindowSize = {400, 400};
+
+// UI Data
+static HGLRC            g_hRC;
+static WGL_WindowData   g_MainWindow;
+static int              g_Width;
+static int              g_Height;
+static bool             g_MouseDownOnHeader;
+static POINT            g_LastMousePos;
+
+// Forward declarations of helper functions
+bool CreateDeviceWGL(HWND hWnd, WGL_WindowData* data);
+void CleanupDeviceWGL(HWND hWnd, WGL_WindowData* data);
+void ResetDeviceWGL();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Main code
+int main(int, char**)
+{
+    // Create application window
+    //ImGui_ImplWin32_EnableDpiAwareness();
+    WNDCLASSEXW wc = { sizeof(wc), CS_OWNDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"crypng", nullptr };
+    ::RegisterClassExW(&wc);
+
+    RECT RectWindow;
+	GetWindowRect(GetDesktopWindow(), &RectWindow);
+	int WindowStartX = RectWindow.right / 2 - (k_WindowSize.x / 2);
+	int WindowStartY = RectWindow.bottom / 2 - (k_WindowSize.y / 2);
+
+    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"crypng", WS_POPUP, WindowStartX, WindowStartY, k_WindowSize.x, k_WindowSize.y, nullptr, nullptr, wc.hInstance, nullptr);
+
+    // Initialize OpenGL
+    if (!CreateDeviceWGL(hwnd, &g_MainWindow))
+    {
+        CleanupDeviceWGL(hwnd, &g_MainWindow);
+        ::DestroyWindow(hwnd);
+        ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+        return 1;
+    }
+    wglMakeCurrent(g_MainWindow.hDC, g_hRC);
+
+    // Show the window
+    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+    ::UpdateWindow(hwnd);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;   // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;    // Enable Gamepad Controls
+    io.IniFilename = NULL;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsClassic();
+    //ImGui::StyleColorsClassic();
+
+    // Modify ImGui Style Defaults
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.TabRounding = 8.f;
+    style.FrameRounding = 8.f;
+    style.GrabRounding = 8.f;
+    style.WindowRounding = 8.f;
+    style.PopupRounding = 8.f;
+    style.ChildRounding = 8.f;
+    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.06f, 0.06f, 0.06f, 1.0f);
+    style.AntiAliasedLines = false;
+    style.AntiAliasedFill = false;
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplWin32_InitForOpenGL(hwnd);
+    ImGui_ImplOpenGL3_Init();
+
+    LONG lStyle = GetWindowLong( hwnd, GWL_STYLE );
+    SetWindowLong( hwnd, GWL_STYLE, lStyle & 
+        (~(WS_CAPTION|WS_THICKFRAME|WS_MINIMIZEBOX|WS_SYSMENU ) ) );
+    SetWindowLong( hwnd, GWL_EXSTYLE, GetWindowLong( hwnd, GWL_STYLE ) | WS_EX_LAYERED);
+
+    SetLayeredWindowAttributes(hwnd, COLORREF(RGB(255, 0, 0)), 255, LWA_COLORKEY | LWA_ALPHA);
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(1.0f, 0.0f, 0.0f, 1.00f);
+
+    // Main loop
+    bool done = false;
+
+    while (!done)
+    {
+        // Poll and handle messages (inputs, window resize, etc.)
+        // See the WndProc() function below for our to dispatch events to the Win32 backend.
+        MSG msg;
+        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                done = true;
+        }
+        if (done)
+            break;
+        if (::IsIconic(hwnd))
+        {
+            ::Sleep(10);
+            continue;
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+
+        DWORD dwFlag = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove;
+        static bool open = true;
+
+		if (!open)
+			ExitProcess(0);
+
+
+        ImGui::SetNextWindowSize(ImVec2(k_WindowSize.x, k_WindowSize.y));
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::Begin("crypng", &open, dwFlag); 
+        
+        ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        glViewport(0, 0, g_Width, g_Height);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Present
+        ::SwapBuffers(g_MainWindow.hDC);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+
+    CleanupDeviceWGL(hwnd, &g_MainWindow);
+    wglDeleteContext(g_hRC);
+    ::DestroyWindow(hwnd);
+    ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+
+    return 0;
+}
+
+// Helper functions
+bool CreateDeviceWGL(HWND hWnd, WGL_WindowData* data)
+{
+    HDC hDc = ::GetDC(hWnd);
+    PIXELFORMATDESCRIPTOR pfd = { 0 };
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 32;
+
+    const int pf = ::ChoosePixelFormat(hDc, &pfd);
+    if (pf == 0)
+        return false;
+    if (::SetPixelFormat(hDc, pf, &pfd) == FALSE)
+        return false;
+    ::ReleaseDC(hWnd, hDc);
+
+    data->hDC = ::GetDC(hWnd);
+    if (!g_hRC)
+        g_hRC = wglCreateContext(data->hDC);
+    return true;
+}
+
+void CleanupDeviceWGL(HWND hWnd, WGL_WindowData* data)
+{
+    wglMakeCurrent(nullptr, nullptr);
+    ::ReleaseDC(hWnd, data->hDC);
+}
+
+// Forward declare message handler from imgui_impl_win32.cpp
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+bool mousedown = false;
+POINT lastLocation;
+POINT currentpos;
+
+// Win32 message handler
+// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (wParam != SIZE_MINIMIZED)
+        {
+            g_Width = LOWORD(lParam);
+            g_Height = HIWORD(lParam);
+        }
+        return 0;
+    case WM_LBUTTONDOWN:
+    {
+        GetCursorPos(&g_LastMousePos);
+        RECT RectWindow;
+        GetWindowRect(hWnd, &RectWindow);
+        if (g_LastMousePos.y - RectWindow.top < 16)
+        {
+            g_MouseDownOnHeader = true;
+        }
+        break;
+    }
+    case WM_LBUTTONUP:
+    {
+        g_MouseDownOnHeader = false;
+        break;
+    }
+    case WM_MOUSEMOVE:
+    {
+        if (g_MouseDownOnHeader)
+        {
+            POINT CurrentMousePoint;
+            GetCursorPos(&CurrentMousePoint);
+            int xDiff = CurrentMousePoint.x - g_LastMousePos.x;
+            int yDiff = CurrentMousePoint.y - g_LastMousePos.y;
+            
+            RECT RectWindow;
+            GetWindowRect(hWnd, &RectWindow);
+
+            SetWindowPos(hWnd, nullptr, RectWindow.left + xDiff, RectWindow.top + yDiff, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+
+            g_LastMousePos = CurrentMousePoint;
+        }
+        break;
+    }
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    }
+    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
+}
